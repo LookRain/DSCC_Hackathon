0eb5b5893ef99cd6a7d241477d32e52fd31bd552
LookRain <brantscube@hotmail.com>
2017-05-06 21:03:14 +0800

    ini commit

---
+++ patronus/database/migrations/2017_05_06_123427_create_scores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('owner')->unsigned();
+            $table->float('score');
+            $table->timestamps();
+
+            $table->foreign('owner')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
